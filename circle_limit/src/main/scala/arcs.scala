b64ac7afede94a279e707bb9b585580dd0adfbe4
a <4184070+MrCurtis@users.noreply.github.com>
2015-08-16 19:10:54 +0100

    Create a simple MoebiusTransformation class.
    Objects of the class can be composed with themselves, and can be used
    to transform single points.

--- circle_limit/src/main/scala/arcs.scala
+++ circle_limit/src/main/scala/arcs.scala
@@ -1,8 +1,17 @@
 package circle_limit
 
-import spire.implicits._
-import breeze.math.Complex
-import breeze.math._
+import breeze.linalg.{
+  DenseMatrix,
+  sum
+}
+import breeze.math.{
+  Complex, 
+  i
+}
+import breeze.numerics.{
+  sqrt,
+  abs
+}
 
 /**
  * Represents an arc of a circle.
@@ -11,4 +20,93 @@ import breeze.math._
  * be used to represent the segment of a circle with centre z_2, and 
  * radius |z_0 - z_2| drawn anti-clockwise from z_0 to z_1.
  */   
-case class Arc(val start: Complex, val finish: Complex, val centre: Complex)
+case class Arc(start: Complex, finish: Complex, centre: Complex)
+
+/**
+ * A Moebius transformation of form az+b/cz+d.
+ */
+class MoebiusTransformation(a: Complex, b: Complex, c: Complex, d: Complex) {
+
+  /**
+   * If mt is a MobiusTransformation and z a complex number, then 
+   * mt transform z returns the image of z under mt.
+   */
+  def transform(z: Complex): Complex = (a*z + b) / (c*z + d)
+
+  /**
+   * Returns the composite of the two transformations.
+   */
+  def compose(that: MoebiusTransformation) = {
+    val composedMatrix = this.theTransformationMatrix * that.theTransformationMatrix
+    new MoebiusTransformation(
+      composedMatrix(0,0),
+      composedMatrix(0,1),
+      composedMatrix(1,0),
+      composedMatrix(1,1)
+    )
+  }
+
+  /**
+   * The transformation represented as an element of PSL(2,C).
+   */
+  val theTransformationMatrix = normalize(DenseMatrix((a, b), (c, d)))
+
+  /**
+   * Returns a matrix in it's canonical form as an element of PSL(2,C).
+   *
+   * Canonical representives of a matrix in PSL(2,C) have determinant
+   * equal to one.
+   */
+  private def normalize(unnormalized: DenseMatrix[Complex]): DenseMatrix[Complex] = {
+    unnormalized / srt(det(unnormalized))
+  }
+
+  /**
+   * Returns the principal sqaureroot of a complex number.
+   *
+   * The principal squareroot is that which is contained in the upper 
+   * half-plane union the real line.
+   */
+  private def srt(num: Complex): Complex = {
+    if (num.imag == 0 && num.re <= 0) {
+      0.0 + sqrt (-1.0 * num.re) * i
+    }
+    else {
+      val r = abs(num)
+      sqrt(r) * (num + r)/(abs(num + r))
+    }
+      
+  }
+
+  /**
+   * Returns the determinant of a 2x2 matrix
+   */
+  private def det(matrix: DenseMatrix[Complex]): Complex = {
+    matrix(0,0)*matrix(1,1) - matrix(0,1)*matrix(1,0)
+  }
+
+  /**
+   * The standard norm on PSL(2,C)
+   */
+  private def matrixNorm(matrix: DenseMatrix[Complex]): Double = {
+    sqrt(
+      sum(
+        matrix.map(z => z.abs*z.abs)
+      )
+    )
+  }
+
+  /**
+   * Equality is defined up to equivalence class of PSL(2,C)
+   *
+   * To avoid problems due to rounding errors we consider two transformations
+   * to be equal when they are 'close' with respect to the standard norm
+   * on PSL(2,C)
+   */
+  def equal(that: MoebiusTransformation): Boolean = {
+    val errorBounds = 0.0000000000001
+    val mt1 = this.theTransformationMatrix + that.theTransformationMatrix
+    val mt2 = this.theTransformationMatrix - that.theTransformationMatrix
+    (matrixNorm(mt1) < errorBounds) ||  (matrixNorm(mt2) < errorBounds)
+  }
+}
--- circle_limit/src/test/scala/arc_test.scala
+++ circle_limit/src/test/scala/arc_test.scala
@@ -2,6 +2,7 @@ package circle_limit
 
 import utest._
 import breeze.math._
+import breeze.math._
 
 object ArcTestSuite extends TestSuite{
   val tests = TestSuite{
@@ -10,9 +11,54 @@ object ArcTestSuite extends TestSuite{
     }
     "values are accesible"-{
       val arc = Arc(1.0+2.0*i, 3.0+4.0*i, 5.0+6.0*i)
-      arc.start == 1.0+2.0*i
-      arc.finish == 3.0+4.0*i
-      arc.centre == 5.0+6.0*i
+      assert (arc.start == 1.0+2.0*i)
+      assert (arc.finish == 3.0+4.0*i)
+      assert (arc.centre == 5.0+6.0*i)
+    }
+  }
+}
+
+object MobiusTransformationTestSuite extends TestSuite{
+  val tests = TestSuite{
+
+    "applying a transformation to a complex number returns the correct result"-{
+      // TODO - work out why implicit conversion does not work here.
+      val t = new MoebiusTransformation(1.0+0*i, 1.0+0*i, 0.0+0*i, 1.0+0*i)
+      val z = 1.0 + 1.0*i
+      val w = t transform z
+      assert (w == 2.0 + 1.0*i)
+    }
+
+    "equality is determined as an element of PSL(2,C)"-{
+      val t1 = new MoebiusTransformation(1.0+0.0*i, 2.0-2.0*i, 5.0+0.0*i, 7.0+0.0*i)
+      val t2 = new MoebiusTransformation(2.0+0.0*i, 4.0-4.0*i, 10.0+0.0*i, 14.0+0.0*i)
+      val t3 = new MoebiusTransformation(-1.0+0.0*i, -2.0+2.0*i, -5.0+0.0*i, -7.0+0.0*i)
+      assert (t1 equal t2)
+      assert (t1 equal t3)
+    }
+
+    "transformations which are 'close' norm-wise are treated as equal - to ignore rounding"-{
+      val t1 = new MoebiusTransformation(
+        -0.3062072401072272 + 0.04494168029824927*i,
+        1.0085050809181804 + 0.4775894393197067*i,
+        -1.5310362005361362 + 0.22470840149124635*i,
+        1.845627962623881 + 1.9187422792593443*i
+      )
+      val t2 = new MoebiusTransformation(
+        -0.3062072401072272 + 0.04494168029824924*i,
+        1.0085050809181801 + 0.4775894393197067*i,  
+        -1.531036200536136 + 0.22470840149124627*i,
+        1.8456279626238805 + 1.9187422792593445*i  
+      )
+      assert (t1 equal t2)
+    }
+
+    "composing two transformations results in the correct transformation"-{
+      val t1 = new MoebiusTransformation(1.0+0.0*i, 2.0-2.0*i, 5.0+0.0*i, 7.0+0.0*i)
+      val t2 = new MoebiusTransformation(-1.0+0.0*i, 1.0+0.0*i, 0.0+0.0*i, 1.0*i)
+      val expected = new MoebiusTransformation(-1.0+0.0*i, 3.0+2.0*i, -5.0+0.0*i, 5.0 + 7.0*i)
+      val composed = t1 compose t2
+      assert (composed equal expected)
     }
   }
 }
