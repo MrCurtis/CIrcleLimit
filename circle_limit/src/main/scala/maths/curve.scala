eaabac5ac278ff0efd886616f4d4155830e5090e
a <4184070+MrCurtis@users.noreply.github.com>
2015-09-06 00:54:58 +0100

    Add equals operator to Line and Arc.

--- circle_limit/src/main/scala/maths/curve.scala
+++ circle_limit/src/main/scala/maths/curve.scala
@@ -7,7 +7,21 @@ abstract class Curve(val start: Complex, val finish: Complex)
 /**
  * Represents a Euclidean line, with end-points start and finish.
  */
-class Line(start: Complex, finish: Complex) extends Curve(start, finish)
+class Line(start: Complex, finish: Complex) extends Curve(start, finish) {
+
+  def canEqual(other: Any) = other.isInstanceOf[Line]
+
+  // TODO - not sure if this is a good hashcode, but any replacement 
+  // must be symmetrical.
+  override def hashCode: Int = start.hashCode + finish.hashCode
+
+  override def equals (that: Any) = that match {
+    case that: Line =>
+      (that canEqual this) &&
+      ( (that.start == this.start && that.finish == this.finish) || 
+        (that.start == this.finish && that.finish == this.start))
+  }
+}
 object Line {
   def apply(start: Complex, finish: Complex) = new Line(start, finish)
 }
@@ -19,7 +33,26 @@ object Line {
  * be used to represent the segment of a circle with centre z_2, and 
  * radius |z_0 - z_2| drawn anti-clockwise from z_0 to z_1.
  */   
-class Arc(start: Complex, finish: Complex, val centre: Complex) extends Curve(start, finish)
+class Arc(start: Complex, finish: Complex, val centre: Complex) extends Curve(start, finish) {
+
+  def canEqual(other: Any) = other.isInstanceOf[Arc]
+
+  override def hashCode: Int = 
+    41 * (
+      41 * (
+        41 + start.hashCode
+      ) + finish.hashCode
+    ) + centre.hashCode
+
+  override def equals(other: Any) = other match {
+    case that: Arc => 
+      (that canEqual this) &&
+      that.start == this.start &&
+      that.finish == this.finish &&
+      that.centre == this.centre
+  }
+        
+}
 object Arc {
   def apply(start: Complex, finish: Complex, centre: Complex) = new Arc(start, finish, centre)
 }
--- circle_limit/src/test/scala/maths/curve_test.scala
+++ circle_limit/src/test/scala/maths/curve_test.scala
@@ -16,6 +16,27 @@ object LineTestSuite extends TestSuite {
       assert (line.finish == 3.0+4.0*i)
     }
 
+    "two lines are equal if and only if the two end points are equal"-{
+      val line1 = Line(1.0+2.0*i, 3.0+4.0*i)
+      val line2 = Line(1.0+2.0*i, 3.0+4.0*i)
+      val line3 = Line(3.0+4.0*i, 1.0+2.0*i)
+      val line4 = Line(3.7+4.3*i, 8.0-2.0*i)
+      val line5 = Line(1.0+2.0*i, 8.0-2.0*i)
+      val line6 = Line(3.7+4.3*i, 3.0+4.0*i)
+
+      assert (line1 == line1)
+      assert (line1 == line2)
+      assert (line2 == line1)
+      assert (line1 == line3)
+      assert (line3 == line1)
+      assert (line1 != line4)
+      assert (line4 != line1)
+      assert (line1 != line5)
+      assert (line5 != line1)
+      assert (line1 != line6)
+      assert (line6 != line1)
+    }
+
   }
 }
 
@@ -31,6 +52,24 @@ object ArcTestSuite extends TestSuite{
       assert (arc.start == 1.0+2.0*i)
       assert (arc.finish == 3.0+4.0*i)
       assert (arc.centre == 5.0+6.0*i)
+    }
+
+    "are equal only if centre and end points match"-{
+      val arc1 = Arc(-1.0+0.0*i, 0.0+1.0*i, 0.0+0.0*i)
+      val arc2 = Arc(-1.0+0.0*i, 0.0+1.0*i, 0.0+0.0*i)
+      val arc3 = Arc(1.0+0.0*i, 0.0+1.0*i, 0.0+0.0*i)
+      val arc4 = Arc(-1.0+0.0*i, 1.0+0.0*i, 0.0+0.0*i)
+      val arc5 = Arc(-1.0+0.0*i, 0.0+1.0*i, -1.0+1.0*i)
+
+      assert (arc1 == arc1)
+      assert (arc1 == arc2)
+      assert (arc2 == arc1)
+      assert (arc1 != arc3)
+      assert (arc3 != arc1)
+      assert (arc1 != arc4)
+      assert (arc4 != arc1)
+      assert (arc1 != arc5)
+      assert (arc5 != arc1)
     }
 
   }
