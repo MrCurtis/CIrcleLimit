7ef4ad9134b39f8065051bc2dda6761a560e3e7d
a <4184070+MrCurtis@users.noreply.github.com>
2016-02-28 18:02:53 +0000

    Fix unstashing problems

--- circle_limit/shared/src/main/scala/CircleLimitApp.scala
+++ circle_limit/shared/src/main/scala/CircleLimitApp.scala
@@ -5,11 +5,39 @@ import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
 import scala.scalajs.js.DynamicImplicits.number2dynamic
 import org.scalajs.dom.html
+import spire.implicits._
+
+import circle_limit.maths.{
+  Arc,
+  Line
+}
+import circle_limit.maths.Imaginary.i
+import circle_limit.graphics.ArcPlotter
 
 @JSExport
 object CircleLimitApp {
   @JSExport
   def main(movie: html.Div): Unit = {
+
+    val transform = ArcPlotter.convertFromMathematicalToGraphicalSpace(
+      (-1.1, -1.1),
+      2.2,
+      2.2,
+      (0.0, 0.0),
+      500, //magic number
+      400, //magic number
+      _ : (Double, Double)
+    )
+    val inputList = List(
+      Arc(-1.0+0.0*i, 0.0-1.0*i, 0.0+0.0*i),
+      Line(0.0+1.0*i, 1.0+0.0*i),
+      Arc(-1.0+1.0*i, 1.0+1.0*i, 0.0+1.0*i)
+    )
+    val curveString = ArcPlotter.createConstructorStringFromList(
+      inputList,
+      transform
+    )
+
     val document = js.Dynamic.global.document
     val playground = document.getElementById("movie")
 
@@ -21,7 +49,7 @@ object CircleLimitApp {
     bonsai.run(
       movie,
       js.Dynamic.literal(
-        code = "new Rect(10, 10, 100, 100).addTo(stage).attr('fillColor', 'green'); new Rect(10, 120, 100, 100).addTo(stage).attr('fillColor', 'red'); new Rect(10, 230, 100, 100).addTo(stage).attr('fillColor', 'blue');",
+        code = curveString,
         width = 500,
         height = 400 
       )
