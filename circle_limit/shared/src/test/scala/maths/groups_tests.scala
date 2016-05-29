4f8c0d5353d0021883b333efd454d74040247900
a <4184070+MrCurtis@users.noreply.github.com>
2016-05-29 21:48:02 +0100

    Added elements attribute to Group.
    Set of Moebius transformations are now created once at object
    creation, and stored as an attribute.

--- circle_limit/shared/src/test/scala/maths/groups_tests.scala
+++ circle_limit/shared/src/test/scala/maths/groups_tests.scala
@@ -14,7 +14,7 @@ object GroupTestSuite extends TestSuite {
 
   val tests = TestSuite {
     
-    "allWordsOfLength(3) returns correct elements for a torsion-free group" - {
+    "with wordLength=3, elements returns correct elements for a torsion-free group" - {
       val identity = MoebiusTransformation(1.0, 0.0, 0.0, 1.0)
       val transform1 = MoebiusTransformation(3.0, 2.0+1.0*i, 2.0-1.0*i, 3.0)
       val transform1Inverse = MoebiusTransformation(3.0, -2.0-1.0*i, -2.0+1.0*i, 3.0)
@@ -82,19 +82,9 @@ object GroupTestSuite extends TestSuite {
         transform2Inverse compose transform2Inverse compose transform1Inverse
       )
 
-      val group = Group(List(transform1, transform2))
-      val returned = group.allWordsOfLength(3)
-
-      println ("Debugging stuff: ")
-      //print ("expected size: ")
-      //print (expected.size)
-      //print ("expected: ")
-      //print (expected.toString)
-      //println ("")
-      //print ("returned: ")
-      //print (returned.toString)
-      print ("returned size: ")
-      print (returned.size)
+      val wordLength = 3
+      val group = Group(List(transform1, transform2), wordLength)
+      val returned = group.elements
 
       assert ( checkSetsOfMoebiusTransformationAlmostEqual ( expected, returned ) )
     } 
