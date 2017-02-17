0a879997c84aa9e452df032705b474fdb0fa0239
a <4184070+MrCurtis@users.noreply.github.com>
2017-02-17 13:51:37 +0100

    Add selenium test to check page title

--- circle_limit/jvm/src/test/scala/selenium_test.scala
+++ circle_limit/jvm/src/test/scala/selenium_test.scala
@@ -1,16 +1,24 @@
 package circle_limit
-import utest._
 
-import org.openqa.selenium.WebDriver
+import utest._
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 object SeleniumTestSuite extends TestSuite {
+
   val tests = TestSuite {
 
-    "first attempt"-{
-      val driver = new FirefoxDriver()
-      assert (true)
-    }
+    val fileUrl = "file:///vagrant/circle_limit/circle_limit.html"
 
+    "title should be \"Circle Limit\""-{
+      val driver = new FirefoxDriver()
+      try{
+        driver.get(fileUrl)
+        val expectedTitle = "Circle Limit"
+        val actualTitle = driver.getTitle
+        assert (expectedTitle == actualTitle)
+      } finally {
+        driver.close()
+      }
+    }
   }
 }
